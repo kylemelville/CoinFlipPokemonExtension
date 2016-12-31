50a648f448f0d92f259809a3f4fe7a957c8cb0f6
Kyle Melville <KPMelville@gmail.com>
2016-12-30 20:22:14 -0600

    Adds Green Zygarde and Red Mega Gyrados coins

--- app/build/generated/source/r/debug/com/banasiak/coinflipext/example/R.java
+++ app/build/generated/source/r/debug/com/banasiak/coinflipext/example/R.java
@@ -15,20 +15,26 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int e33253fc1989462ab1f9b109c520fc7d_edge=0x7f020000;
-        public static final int e33253fc1989462ab1f9b109c520fc7d_heads=0x7f020001;
-        public static final int e33253fc1989462ab1f9b109c520fc7d_tails=0x7f020002;
-        public static final int e680946a1ae44369b98f00183108db28_edge=0x7f020003;
-        public static final int e680946a1ae44369b98f00183108db28_heads=0x7f020004;
-        public static final int e680946a1ae44369b98f00183108db28_tails=0x7f020005;
-        public static final int edge=0x7f020006;
-        public static final int g54b3e8aeea54976a2edc04e90dc61a5_edge=0x7f020007;
-        public static final int g54b3e8aeea54976a2edc04e90dc61a5_heads=0x7f020008;
-        public static final int g54b3e8aeea54976a2edc04e90dc61a5_tails=0x7f020009;
-        public static final int icon=0x7f02000a;
-        public static final int pokemon_edge=0x7f02000b;
-        public static final int pokemon_tails=0x7f02000c;
-        public static final int tails=0x7f02000d;
+        public static final int d7a78b695e844cac883694bf40b279fa_edge=0x7f020000;
+        public static final int d7a78b695e844cac883694bf40b279fa_heads=0x7f020001;
+        public static final int d7a78b695e844cac883694bf40b279fa_tails=0x7f020002;
+        public static final int e33253fc1989462ab1f9b109c520fc7d_edge=0x7f020003;
+        public static final int e33253fc1989462ab1f9b109c520fc7d_heads=0x7f020004;
+        public static final int e33253fc1989462ab1f9b109c520fc7d_tails=0x7f020005;
+        public static final int e680946a1ae44369b98f00183108db28_edge=0x7f020006;
+        public static final int e680946a1ae44369b98f00183108db28_heads=0x7f020007;
+        public static final int e680946a1ae44369b98f00183108db28_tails=0x7f020008;
+        public static final int edge=0x7f020009;
+        public static final int f9cae597281a40d48215c9c13872e638_edge=0x7f02000a;
+        public static final int f9cae597281a40d48215c9c13872e638_heads=0x7f02000b;
+        public static final int f9cae597281a40d48215c9c13872e638_tails=0x7f02000c;
+        public static final int g54b3e8aeea54976a2edc04e90dc61a5_edge=0x7f02000d;
+        public static final int g54b3e8aeea54976a2edc04e90dc61a5_heads=0x7f02000e;
+        public static final int g54b3e8aeea54976a2edc04e90dc61a5_tails=0x7f02000f;
+        public static final int icon=0x7f020010;
+        public static final int pokemon_edge=0x7f020011;
+        public static final int pokemon_tails=0x7f020012;
+        public static final int tails=0x7f020013;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
